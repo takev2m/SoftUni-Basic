58e0e126f725d9de9d4b1022b62edf05201f4769
takev2m <risite@abv.bg>
2016-03-24 19:42:32 +0200

    Added files via upload

---
+++ homeworks/02/PrintSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PrintSequence
+{
+    static void Main()
+    {
+        int numbers;
+        for (int i = 2; i < 12; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write("{0},", i);
+            }
+            else
+            {
+                Console.Write("{0},", -i);
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ homeworks/02/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        double a = Math.Sqrt(12345);
+        Console.WriteLine(a);
+    }
+}
