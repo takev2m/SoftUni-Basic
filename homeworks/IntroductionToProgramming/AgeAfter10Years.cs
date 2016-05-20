35799b08aef1038398802e0aedc57a51406dbefb
takev2m <risite@abv.bg>
2016-05-20 15:25:15 +0300

    Update AgeAfter10Years.cs

--- homeworks/IntroductionToProgramming/AgeAfter10Years.cs
+++ homeworks/IntroductionToProgramming/AgeAfter10Years.cs
@@ -5,6 +5,7 @@ class AgeAfter10Years
     static void Main()
     {
         DateTime today = new DateTime(2015, 11, 01);
+        
         Console.Write("Write down your Birthday YYYY-MM-DD: ");
         DateTime birthdate = DateTime.Parse(Console.ReadLine()); 
         Console.WriteLine("Now: " + (today.Year - birthdate.Year));
